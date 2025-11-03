2d40d3b0f8fb0b85fa8860c48730d9fce2a75c0a
NguyenHoangNhatKhang <nghoangnhkhang@gmail.com>
2025-11-03 10:56:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def chat_answer():
     return jsonify({"reply": reply})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False  )
